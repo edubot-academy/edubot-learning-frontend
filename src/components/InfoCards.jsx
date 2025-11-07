e2e82179352ccbb8e259cba7010e08017e69ddda
kulpunai180 <jekshenbekova@outlook.com>
2025-11-07 21:49:52 +0600

    in progress

---
+++ src/components/InfoCards.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function InfoCards() {
+  return (
+    <div>
+      <div>
+        <img src='' alt="" />
+        <h3></h3>
+        <p>
+            EduBot Learning — бул ар бир адамга ылайыкталган онлайн билим берүү платформасы. Биздин максат – заманбап, жеткиликтүү жана сапаттуу билимди дүйнө жүзүндөгү бардык адамдарга жеткирүү.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default InfoCards
