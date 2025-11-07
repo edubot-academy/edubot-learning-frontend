559b668dbe5f77b3a7170b06fa80a07b9c5ea9e9
kulpunai180 <jekshenbekova@outlook.com>
2025-11-07 17:49:44 +0600

    finish metrics in about us

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import AboutImage from '../components/AboutImage';
 import AboutText from '../components/AboutText';
+import Metrics from '../components/Metrics';
 
 const AboutPage = () => {
     return (
         <div className="bg-gray-50 min-h-screen py-24 text-gray-800"> {/* py-24 instead of py-10 to push content below header */}
             <AboutImage />
+            <Metrics />
             <AboutText />
         </div>
     );
