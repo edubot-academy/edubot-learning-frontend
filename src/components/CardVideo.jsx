2b7aa084327a030ae83084911714694476233e51
kulpunai180 <jekshenbekova@outlook.com>
2025-11-14 17:44:03 +0600

    changed the component name

--- src/components/CardVideo.jsx
+++ src/components/CardVideo.jsx
@@ -5,10 +5,9 @@ import { TbLock } from "react-icons/tb";
 import Button from "../components/ui/button";
 import { FaPlay } from "react-icons/fa";
 
-const CourseCard = () => {
+const CardVideo = () => {
     return (
         <div className="border border-gray-200 rounded-md overflow-hidden bg-white w-[90vw] sm:w-[70vw] md:w-[40vw] lg:w-[30vw] max-w-[420px] px-4 py-6 m-5">
-            {/* Изображение */}
             <div className="relative w-full ">
                 <img
                     className="w-full h-[30vw] sm:h-[20vw] md:h-[15vw] lg:h-[12vw] object-cover rounded-md"
@@ -20,9 +19,7 @@ const CourseCard = () => {
                         <FaPlay className="text-[#EA580C] text-2xl pl-1" />
                     </button>
                 </div>
-
             </div>
-
             <div className="flex flex-col gap-[1vw] py-[2vw] sm:py-[1.5vw]">
                 <div className="flex items-center justify-between text-gray-700">
                     <p className="text-lg text-[#5A5F69] font-normal">
@@ -32,7 +29,6 @@ const CourseCard = () => {
                         120$
                     </span>
                 </div>
-
                 <div className="flex flex-col gap-[0.5vw] text-[#3E424A]">
                     <p className="flex items-center gap-[0.6vw] text-base font-semibold">
                         <FiBook /> 12 уроков
@@ -44,7 +40,6 @@ const CourseCard = () => {
                         <TbLock /> Доступ платный
                     </p>
                 </div>
-
                 <div className="flex md:flex-col gap-[1vw] mt-[1vw]">
                     <div>
                         <Button>
@@ -56,12 +51,10 @@ const CourseCard = () => {
                             Добавить в избранное
                         </Button>
                     </div>
-
-
                 </div>
             </div>
         </div>
     );
 };
 
-export default CourseCard;
+export default CardVideo;
