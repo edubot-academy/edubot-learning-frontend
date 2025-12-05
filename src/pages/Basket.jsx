14e4adb9b4ebac01886390734fb7a279cc66c34c
Tangsuluu <karybekovatansuluu303@gmail.com>
2025-12-05 15:02:38 +0600

    payment in basket

---
+++ src/pages/Basket.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PaymentCourse from '@features/marketing/PaymentCourse'
+
+function Basket() {
+  return (
+    <div>
+      <PaymentCourse/>
+    </div>
+  )
+}
+
+export default Basket
